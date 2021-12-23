979522646cd85fea0f6d3999081f9a2444dc7422
Alessandro Rossi <75400381+AlessandroRossiUFF@users.noreply.github.com>
2021-12-23 21:37:32 +0000

    frst

--- Main.cs
+++ Main.cs
@@ -1,8 +1,38 @@
-using System; 
+using System;
+using System.Collections.Generic;
+using System.Text;
 
-public class Program {
+namespace Dio
+{
+    class Xenlongao
+    {
+        static void Main(string[] args)
+        {
+            string t = Console.ReadLine();
+            int C = Int32.Parse(t);
+            for(int k=0; k<C; k++)
+            {
+              string n = Console.ReadLine();
+              int N = Int32.Parse(n);
+              int divs=0;
+              for(int j=1; j<=N; j++){
+                int cont=0;
+  
 
-  public static void Main(string[] args) {
-    Console.WriteLine("hello world");
-  }
+
+                for(int i=1; i<=j; i++){
+                  if(j%i==0){
+                    cont++;
+                  }
+                }
+
+                
+                if(cont%2==0){
+                  divs+=1;
+                }
+              }
+              Console.WriteLine(divs);
+            }
+        }
+    }
 }
